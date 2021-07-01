b6e8a75fca06f1abf689266c4c574c1b4b78b195
eslamfaisal <eslam.faisal.ef@gmail.com>
2021-07-01 02:38:05 +0200

    add sim info method channel

--- lib/cell_info.dart
+++ lib/cell_info.dart
@@ -9,4 +9,11 @@ class CellInfo {
     final String version = await _channel.invokeMethod('cell_info');
     return version;
   }
+
+  static const MethodChannel _sim_info = const MethodChannel('sim_info');
+
+  static Future<String> get getSIMInfo async {
+    final String version = await _sim_info.invokeMethod('sim_info');
+    return version;
+  }
 }
